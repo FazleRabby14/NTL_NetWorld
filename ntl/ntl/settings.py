c2acd88d0a53e423b140c77db8f7f6f2cb48e75a
FazleRabby14 <km.fazla@northsouth.edu>
2024-12-19 17:36:59 +0600

    Modified

--- ntl/ntl/settings.py
+++ ntl/ntl/settings.py
@@ -77,28 +77,28 @@ WSGI_APPLICATION = 'ntl.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'ntl',
-#         'USER': 'root',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#         'PASSWORD': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+        
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ntl',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'PASSWORD': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
